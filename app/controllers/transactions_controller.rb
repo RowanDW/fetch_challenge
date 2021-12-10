80135fa60d5cb7525808c771a3543d637a15fe90
Rowan DeLong <rowanwinzer@gmail.com>
2021-12-10 11:52:52 -0700

    Add comments

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -3,6 +3,7 @@ class TransactionsController < ApplicationController
         transaction = Transaction.new(payer: params[:payer], points: params[:points], timestamp: params[:timestamp], remaining_points: params[:points])
         if transaction.save
             if transaction.points < 0
+                # for points coming in as a negative value, points get changed to postive value to be subtracted
                 Transaction.subtract_payer_points((transaction.points * -1), transaction.payer)
                 transaction.update(remaining_points: 0)
             end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -4,6 +4,7 @@ class Transaction < ApplicationRecord
     validates_presence_of(:timestamp)
     validates_presence_of(:remaining_points)
 
+    # Returns all point balances for each payer, used for point_balances endpoint
     def self.point_balances
         payer_points = Transaction.select('payer, SUM(remaining_points) as balance')
             .group(:payer)
@@ -12,10 +13,13 @@ class Transaction < ApplicationRecord
         result_hash
     end
 
+    # Returns total remaining points for all payers
+    # Used by spend points endpoint to determine
     def self.total_points_balance
         sum(:remaining_points)
     end
 
+    # Returns all transactions with postitive remaining points, order by oldest first
     def self.unspent_transactions(target_payer = '')
         if target_payer.present?
             where('remaining_points > 0').where('payer = ?', target_payer).order(:timestamp)
@@ -24,8 +28,12 @@ class Transaction < ApplicationRecord
         end
     end
 
+    # This is used when a user is trying to spend points
+    # Returns a hash of payers and points spent
     def self.spend_points(pts, result = Hash.new(0))
         current_trans = unspent_transactions.first
+        # If spending more points than the oldest transaction, then call method recursively
+        # to spend multiple transactions
         if pts > current_trans.remaining_points
             result[current_trans.payer] -= current_trans.remaining_points
             pts -= current_trans.remaining_points
@@ -39,6 +47,7 @@ class Transaction < ApplicationRecord
         end
     end
 
+    # Only used when adding a negative points transaction
     def self.subtract_payer_points(points, payer)
         current_trans = unspent_transactions(payer).first
         if points > current_trans.remaining_points
@@ -49,6 +58,5 @@ class Transaction < ApplicationRecord
             remaining = current_trans.remaining_points - points
             current_trans.update(remaining_points: remaining)
         end
-
     end
 end
